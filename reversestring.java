f9a26b6de5924eefe8577ce3aef4cf68f1a5a63e
RGK007 <gokulakannanrj@gmail.com>
2017-06-04 09:06:13 +0530

    Update reversestring.java

--- reversestring.java
+++ reversestring.java
@@ -4,22 +4,9 @@ import java.util.*;
 public class reversestring {
    public static void main(String[] args) {
         
-       Scanner s=new Scanner(System.in);
-       System.out.println("Enter the value is integer(1) ot string(0)");
-       int b=s.nextInt();
-       switch(b)
-       {
-           case 0:  String a=s.nextLine();
+      Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
         String s2=new StringBuffer(a).reverse().toString();
-        System.out.println(s2);  
-               break;
-           case 1: String c=s.nextLine();
-               String s4=new StringBuffer(c).reverse().toString();
-        System.out.println(s4);
-               break;
-           default:System.out.println("Invalid");
-       
-       
-    }
+        System.out.println(s2);
    }
 }
