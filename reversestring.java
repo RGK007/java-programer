70b574fa8ff4bbaf12d3a32f2c8962e8712a5143
RGK007 <gokulakannanrj@gmail.com>
2017-06-04 09:08:44 +0530

    Update reversestring.java

--- reversestring.java
+++ reversestring.java
@@ -6,7 +6,13 @@ public class reversestring {
         
       Scanner s=new Scanner(System.in);
         String a=s.nextLine();
+        int b=a.length();
+        if(b>0)
+        {
         String s2=new StringBuffer(a).reverse().toString();
         System.out.println(s2);
+        }
+        else
+        System.out.println("Invalid");
    }
 }
