c8c96fa5e06802f800e613e69225664138a95d5e
RGK007 <gokulakannanrj@gmail.com>
2017-06-05 15:54:09 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class holiday {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        int a=s1.length();
+       if(a>0)
+       if(s1=="sunday")
+        {
+            System.out.println("false");
+        }
+        System.out.println("true");
+        }
+        }
