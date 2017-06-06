a35ba1eb8d49bb3e6afcd7dffac4f3788cec8705
RGK007 <gokulakannanrj@gmail.com>
2017-06-06 20:38:06 +0530

    Create firstrepeated.java

---
+++ firstrepeated.java
@@ -0,0 +1,35 @@
+package checkindex;
+
+	
+import java.util.*;
+import java.util.Scanner;
+	public class firstrepeated
+	{
+	    public static void main(String[] args) 
+	    {
+        int n, flag = 0, count = 0,b = 0;
+	        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+	        int a[] = new int[n];
+	        for(int i = 0; i < n; i++)
+	        {
+	            a[i] = s.nextInt();
+	        }
+	        for(int i = 0; i < n; i++)
+	        {
+	            for(int j = 0; j < n; j++)
+	            {
+                            if(flag==0)
+                            {
+	                if(i != j)
+                    if(a[i] == a[j])
+	                    {
+                                flag++;
+	                      b=a[i];
+                              break;
+                            }
+                    }
+                }
+                }
+                System.out.println(b);
+}    }	
