f23e770232a1f0ae152eec18dcbaf71020f381bf
RGK007 <gokulakannanrj@gmail.com>
2017-09-01 14:24:35 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class reverse {
+
+	public static void main(String[] args) {
+	Scanner sin=new Scanner(System.in);
+	System.out.println("enter the string");
+	String s=sin.nextLine();
+	String[] sb=s.split(" ");
+	for(int i=0;i<sb.length;i++)
+	{
+		StringBuffer s1=new StringBuffer(sb[i]);
+		sb[i]=String.valueOf(s1.reverse());
+	}
+	
+for(int i=0;i<sb.length;i++)
+{
+	System.out.print(sb[i]+" ");
+}
+	}
+
+}
