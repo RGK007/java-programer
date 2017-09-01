b62dc919096ff0af026f685a327cdc904256c2a4
RGK007 <gokulakannanrj@gmail.com>
2017-09-01 14:26:27 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -6,6 +6,8 @@ public class reverse {
 	Scanner sin=new Scanner(System.in);
 	System.out.println("enter the string");
 	String s=sin.nextLine();
+		if(s.length()>0)
+		{
 	String[] sb=s.split(" ");
 	for(int i=0;i<sb.length;i++)
 	{
@@ -17,6 +19,9 @@ for(int i=0;i<sb.length;i++)
 {
 	System.out.print(sb[i]+" ");
 }
+		}
+		else
+			System.out.print("Enter the string correctly");
 	}
 
 }
